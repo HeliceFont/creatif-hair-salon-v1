4d0a9f6c3d3aad40ff2e707c2372da4c01d28ac2
HeliceFont <camacho485@gmail.com>
2024-06-03 03:31:59 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/creatif-hair-salon-v1/',
+  basename: '/creatif-hair-salon-v1/',
   plugins: [react()],
    
 })
