e8365cb3551a857ec2bf266bee4e57de1fb8ff25
HeliceFont <camacho485@gmail.com>
2024-06-20 02:33:31 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,9 @@ export default defineConfig({
     outDir: 'build', // Cambia 'dist' a 'build'
   },
   plugins: [react()],
-   
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js']
+    }
+  }
 })
